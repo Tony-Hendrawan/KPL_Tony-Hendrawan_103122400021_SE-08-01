70b899f6904c10febfd2b1acbb11bee6e9eb4f53
Tony Hendrawan <116004334+Tony-Hendrawan@users.noreply.github.com>
2026-03-12 10:21:07 +0700

    Update exercise.js

--- 02_Pemrograman_JavaScript/GUIDED/exercise.js
+++ 02_Pemrograman_JavaScript/GUIDED/exercise.js
@@ -1,8 +1,11 @@
+//menambahkan value ke array
 const favDrinks = ["jus", "soda", "teh"];
 console.log(favDrinks);
 
+//mengubah value array index ke 1
 favDrinks[1] = "air mineral";
 console.log(favDrinks);
 
 favDrinks.unshift("kopi");
+
 console.log(favDrinks);
