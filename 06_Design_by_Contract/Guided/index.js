0e52bd6f2f02d9d8492bd3d3870e9c0dbc806fa6
Tony Hendrawan <tonyhendrawan4567@email.com>
2026-04-09 15:05:19 +0700

    add modul 6

---
+++ 06_Design_by_Contract/Guided/index.js
@@ -0,0 +1,28 @@
+function divide(a, b) {
+    //prekondisi
+    if (typeof a !== 'number' && typeof b !== 'number') {
+        throw new TypeError("Salah tipe data. Harus bilangan bulat.");
+    }
+
+    if (b === 0) {
+        throw new Error("Tidak dibagi dengan nol.");
+    }
+
+    const hasil = a / b;
+
+    //pascakondisi
+
+    if (hasil * b === a) {
+        return hasil;
+    }
+
+    return hasil;
+
+}
+
+try {
+    console.log(divide(10, 2));
+
+} catch (error) {
+    console.error(error);
+}
